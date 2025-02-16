44c40d2fcb73d647c62b7f318d32a26ce8c6cae8
Heribio <ethan.levratti@icloud.com>
2025-02-16 11:46:09 +0100

    Changed data.json file directory

--- internal/jsonthings/jsonthings.go
+++ internal/jsonthings/jsonthings.go
@@ -2,9 +2,11 @@ package jsonthings
 
 import (
 	"encoding/json"
+    "path/filepath"
 	"errors"
 	"log"
 	"os"
+    _ "embed"
 )
 
 type Data struct {
@@ -12,12 +14,29 @@ type Data struct {
     Tag     string
 }
 
+func getFilePath() string {
+    fileName := "data.json"
+	configDir, err := os.UserConfigDir() // Gets ~/.config on Linux/macOS, %APPDATA% on Windows
+	if err != nil {
+		panic("Could not get user config directory")
+	}
+
+	// Ensure ValTracker directory exists
+	appDir := filepath.Join(configDir, "ValTracker")
+	if err := os.MkdirAll(appDir, os.ModePerm); err != nil {
+		panic("Could not create config directory")
+	}
+
+	return filepath.Join(appDir, fileName)
+}
+
 func GetFileData() Data {
-    if _, err := os.Stat("data.json"); errors.Is(err, os.ErrNotExist) {
-        os.Create("data.json")
-        os.WriteFile("data.json", []byte("{}"), 0666)
+    filePath := getFilePath()
+    if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
+        os.Create(filePath)
+        os.WriteFile(filePath, []byte("{}"), 0666)
     }
-    content, err := os.ReadFile("data.json")
+    content, err := os.ReadFile(filePath)
     if err != nil {
         log.Fatal("Error when opening file: ", err)
     }
@@ -30,8 +49,9 @@ func GetFileData() Data {
 }
 
 func WriteFileData(name string, tag string) {
-    if _, err := os.Stat("data.json"); errors.Is(err, os.ErrNotExist) {
-        os.WriteFile("data.json", []byte("{}"), 0666)
+    filePath := getFilePath()
+    if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
+        os.WriteFile(filePath, []byte("{}"), 0666)
     }
     data := Data{
         Name: name,
@@ -43,7 +63,7 @@ func WriteFileData(name string, tag string) {
         log.Fatal("Error Marshal() data: ", err)
     }
 
-    err = os.WriteFile("data.json", byteValue, 0644)
+    err = os.WriteFile(filePath, byteValue, 0644)
     if err != nil {
         log.Fatal("Error writing data to data.json: ", err)
     }
