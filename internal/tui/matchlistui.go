0933d03b5eb86b867f92d583b82a9ed5c1f38b2d
Heribio <ethan.levratti@icloud.com>
2025-02-19 21:12:33 +0100

    feat: Made matchlistview personnalised (still looks ugly)

--- internal/tui/matchlistui.go
+++ internal/tui/matchlistui.go
@@ -2,6 +2,8 @@ package tui
 
 import (
 	"fmt"
+	"io"
+	"strings"
 
 	"github.com/Heribio/ValTracker/internal/valorantapi"
 	_ "github.com/Heribio/ValTracker/internal/valorantapi"
@@ -27,6 +29,8 @@ func (m model) matchListUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
         case "<":
             m = m.SwitchPage(loginPage)
             return m, nil
+        case "m":
+            return m, nil
        }
     }
 
@@ -45,33 +49,89 @@ func (m model) matchListView() string {
 
 //Display of informations in list items
 func (m Match) Title() string {
-    return fmt.Sprintf("%d-%d %s - %s",m.BlueTeamScore, m.RedTeamScore, m.MapName, m.CharacterName)
+    if (m.Team == "Blue") {
+        if (m.BlueTeamScore > m.RedTeamScore){
+            return fmt.Sprintf("Win - %d-%d %s - %s",m.BlueTeamScore, m.RedTeamScore, m.MapName, m.CharacterName)
+        }
+        if (m.BlueTeamScore < m.RedTeamScore){
+            return fmt.Sprintf("Loss - %d-%d %s - %s",m.BlueTeamScore, m.RedTeamScore, m.MapName, m.CharacterName)
+        } else {
+            return fmt.Sprintf("Draw- %d-%d %s - %s",m.BlueTeamScore, m.RedTeamScore, m.MapName, m.CharacterName)
+        }
+    } else {
+        if (m.BlueTeamScore < m.RedTeamScore){
+            return fmt.Sprintf("Win - %d-%d %s - %s",m.RedTeamScore, m.BlueTeamScore, m.MapName, m.CharacterName)
+        }
+        if (m.BlueTeamScore > m.RedTeamScore){
+            return fmt.Sprintf("Loss - %d-%d %s - %s",m.RedTeamScore, m.BlueTeamScore, m.MapName, m.CharacterName)
+        } else {
+            return fmt.Sprintf("Draw- %d-%d %s - %s",m.RedTeamScore, m.BlueTeamScore, m.MapName, m.CharacterName)
+        }
+    }
 }
 
 func (m Match) Description() string {
     return fmt.Sprintf("Kills: %d | Deaths: %d | Assists: %d | ACS: %d", m.Kills, m.Deaths, m.Assists, m.Score/(m.BlueTeamScore+m.RedTeamScore))
 }
-func (i Match) FilterValue() string { return i.Id}
+func (i Match) FilterValue() string { return i.StartedAt }
+
+var(
+    winStyle = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), false, false, false, true).Foreground(lipgloss.Color("#4bff27")).Bold(true).Padding(0, 2)
+    lossStyle = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), false, false, false, true).Foreground(lipgloss.Color("#f7659")).Bold(true).Padding(0, 2)
+    selectedStyle = lipgloss.NewStyle().Bold(true).Padding(0, 2)
+	cursorStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("6")).Bold(true)
+	descStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Italic(true).PaddingLeft(4)
+	separator      = lipgloss.NewStyle().Foreground(lipgloss.Color("8")).Render("──────────────────────────")
+)
+
+type matchlistDelegate struct{}
+
+func (d matchlistDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	it, ok := listItem.(Match)
+	if !ok {
+		return
+	}
+
+	title := it.Title()
+    description := it.Description()
+	var style lipgloss.Style
+
+	if index == m.Index(){
+        style = selectedStyle
+	} else if strings.Contains(strings.ToLower(title), "win") {
+		style = winStyle
+    } else {
+		style = lossStyle
+	}
+
+	cursor := "  " // No cursor for unselected items
+	if index == m.Index() {
+		cursor = cursorStyle.Render(">") // Highlight selected item
+	}
+
+	fmt.Fprintf(w, "%s %s\n%s\n", cursor, style.Render(title), descStyle.Render(description))
+}
+
+
+func (d matchlistDelegate) Height() int  { return 3 }
+func (d matchlistDelegate) Spacing() int { return 1 }
+func (d matchlistDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd{
+	return nil
+}
 
 //Get matches
 func MatchList(name string, tag string) matchListState {
-    //TODO Get name and tag from loginui
     puuid := valorantapi.GetAccountPUUID(name, tag)
-    resp := valorantapi.GetAccountMatches(puuid)
-    matchList := valorantapi.FormatMatches(resp)
+    matchList := valorantapi.GetAccountMatches(puuid, "1",  "eu", "competitive")
 
     items := make([]list.Item, len(matchList))
     for i := range len(matchList){
         items[i] = Match(matchList[i])
     }
 
-    // winColor := lipgloss.Color("#4bff27")
-    // lossColor = lipgloss.Color("f7659")
-
     windowWidth, windowHeight := 50, 45
-    d := list.NewDefaultDelegate()
-    // d.Styles.SelectedTitle = d.Styles.SelectedTitle.Foreground(winColor)
+    l := list.New(items, matchlistDelegate{}, windowWidth, windowHeight)
 
-    m := matchListState{list: list.New(items, d, windowWidth, windowHeight)}
+    m := matchListState{list: l}
     return m
 }
